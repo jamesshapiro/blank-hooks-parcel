4d86ee7c2a5fb43b984e0577a8f228ed61ce9ede
James Shapiro <jamesshapiro@gmail.com>
2024-03-21 23:12:46 -0400

    add many hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,20 @@ import * as style from './Header.module.css';
 
 function Header() {
   return (
-    <header
-      className={`${style.header} ${true ? style.headerSuccess : ''}`}
-    >
-      <h1>Header</h1>
-    </header>
+    <>
+      <header
+        className={`${style.header} ${true ? style.headerSuccess : ''}`}
+      >
+        <h1>James Shapiro</h1>
+
+      </header>
+      <span>Invisible
+      <span className={style.invisiblePlaceholder}>PLACEHOLDER</span>
+      placeholder
+      <span className={style.invisiblePlaceholder}>DEMO</span>
+      Demo</span>
+    </>
+
   );
 }
 
