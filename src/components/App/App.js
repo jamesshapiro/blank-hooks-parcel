c0803983f79b4491fa245b365c3416b9a55b1d96
James Shapiro <jamesshapiro@gmail.com>
2024-07-24 04:16:14 -0400

    add optional auth support

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,17 @@ import Header from '../Header';
 import React from 'react';
 
 import DataProvider from '../DataProvider';
+import UserProvider from '../UserProvider';
 
 function App() {
   return (
+    // <UserProvider>
     <DataProvider>
-      <div className="wrapper">
+      <div className='wrapper'>
         <Header />
       </div>
     </DataProvider>
+    // </UserProvider>
   );
 }
 
